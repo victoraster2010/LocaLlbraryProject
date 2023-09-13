34bd1edda48f97f708d441d21407ffbe4cc7ef18
Victor Hugo Souza <victoraster2014@gmail.com>
2023-09-13 06:54:17 -0300

    fix:including fixes for some missing stuff

--- src/app.js
+++ src/app.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const asyncHandler = require('express-async-handler');
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const catalogRouter = require("./routes/catalog"); //Import routes for "catalog" area of site
+
+
 
 const app = express();
 
 app.use(express.json());
 //declare your routes/ middlewares here
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/catalog", catalogRouter); // Add catalog routes to middleware chain.
 
 
 module.exports = app
